8d9976678931ace3c70e0b4bafd859b49108861a
Brian Donovan-Smith <brian.donovansmith@gmail.com>
2014-05-03 22:27:08 +1200

    Initial Commit

---
+++ DynamicStyle/DynamicStyle/Dynamic.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DynamicStyle
+{
+    public interface IDynamicRule
+    {
+        string Name { get; set; }
+
+        bool IsMatch(Size size);
+    }
+
+    public class ShapeRule : IDynamicRule
+    {
+        public string Name { get; set; }
+
+        public bool IsMatch(Size size)
+        {
+            if ((MinHeight > 0) && (size.Height < MinHeight))
+                return false;
+            if ((MaxHeight > 0) && (size.Height > MaxHeight))
+                return false;
+            if ((MinWidth > 0) && (size.Width < MinWidth))
+                return false;
+            if ((MaxWidth > 0) && (size.Width > MaxWidth))
+                return false;
+            if (IsWide && (size.Height >= size.Width))
+                return false;
+            if (IsTall && (size.Width >= size.Height))
+                return false;
+
+            return true;
+        }
+
+        public int MinWidth { get; set; }
+        public int MaxWidth { get; set; }
+        public int MinHeight { get; set; }
+        public int MaxHeight { get; set; }
+
+        public bool IsWide { get; set; }            // Landscape
+        public bool IsTall { get; set; }            // Portrait
+    }
+
+    public class Dynamic
+    {
+        public static List<IDynamicRule> DefaultStyleRules = new List<IDynamicRule>
+        {
+            new ShapeRule { Name = "XWide", IsWide = true, MinWidth = 1366 },
+            new ShapeRule { Name = "Wide", IsWide = true },
+            new ShapeRule { Name = "Tall", IsTall = true }
+        };
+
+        private static List<FrameworkElement> FrameworkElementsWithStyle = new List<FrameworkElement>();
+        private static bool WindowSizeChangedHandled = false;
+
+        public static List<IDynamicRule> StyleRules { get; set; }
+
+
+        /*******************************************************************/
+
+        public static readonly DependencyProperty StyleProperty = DependencyProperty.RegisterAttached(
+            "Style", 
+            typeof(string), 
+            typeof(Dynamic), 
+            new PropertyMetadata(default(string)));
+
+        public static void SetStyle(DependencyObject element, string value)
+        {
+            element.SetValue(StyleProperty, value);
+            var frameworkElement = element as FrameworkElement;
+            if (frameworkElement == null) return;
+
+            frameworkElement.Loaded += (sender, args) => FrameworkElementsWithStyle.Add(frameworkElement);
+            frameworkElement.Unloaded += (sender, args) => FrameworkElementsWithStyle.Remove(frameworkElement);
+
+            EnsureWindowSizeChanged();
+
+            var rules = StyleRules ?? DefaultStyleRules;
+
+            var name = GetRuleName(rules, new Size(Window.Current.Bounds.Width, Window.Current.Bounds.Height));
+            SetFrameworkStyle(frameworkElement, name);
+        }
+
+        public static string GetStyle(DependencyObject element)
+        {
+            return (string)element.GetValue(StyleProperty);
+        }
+
+        /*******************************************************************/
+
+        public static readonly DependencyProperty StyleRulesProperty = DependencyProperty.RegisterAttached(
+            "StyleRules", typeof (List<IDynamicRule>), typeof (Dynamic), new PropertyMetadata(new List<IDynamicRule>()));
+
+        public static void SetStyleRules(DependencyObject element, List<IDynamicRule> rules)
+        {
+            StyleRules = rules ?? DefaultStyleRules;
+        }
+
+        public static List<IDynamicRule> GetStyleRules(DependencyObject element)
+        {
+            return StyleRules ?? DefaultStyleRules;
+        }
+
+        /*******************************************************************/
+
+        public static readonly DependencyProperty VisualStateRulesProperty = DependencyProperty.RegisterAttached(
+            "VisualStateRules", typeof (List<IDynamicRule>), typeof (Dynamic), new PropertyMetadata(new List<IDynamicRule>()));
+
+
+        /*
+        public static void SetVisualStateRules(DependencyObject element, List<ShapeRule> rules)
+        {
+            element.SetValue(VisualStateRulesProperty, rules);
+            ValidateRules(rules);
+
+            var control = element as Control;
+            if (control == null) return;
+
+            control.Loaded += (sender, args) =>
+            {
+                control.SizeChanged += ElementOnSizeChanged;
+                UpdateDynamicVisualState(control);
+            };
+            control.Unloaded += (sender, args) => control.SizeChanged -= ElementOnSizeChanged;
+        }
+        */
+
+        public static List<IDynamicRule> GetVisualStateRules(DependencyObject element)
+        {
+            var list = (element.GetValue(VisualStateRulesProperty) as List<IDynamicRule>) ?? new List<IDynamicRule>();
+
+            var control = element as Control;
+            if (control == null) return list;
+
+            control.Loaded += (sender, args) =>
+            {
+                control.SizeChanged += ElementOnSizeChanged;
+                UpdateDynamicVisualState(control);
+            };
+            control.Unloaded += (sender, args) => control.SizeChanged -= ElementOnSizeChanged;
+
+            return list;
+        }
+
+
+        /// Whenever an element is resized, then evaluate
+        private static void ElementOnSizeChanged(object sender, SizeChangedEventArgs sizeChangedEventArgs)
+        {
+            var control = sender as Control;
+            if (control == null) return;
+
+            UpdateDynamicVisualState(control);
+        }
+
+
+        /// Set the visual state based on the controls size
+        public static void UpdateDynamicVisualState(Control control)
+        {
+            var rules = GetVisualStateRules(control);
+            if ((rules == null) || (rules.Count == 0)) return;
+
+            var width = (Math.Abs(control.ActualWidth) > 0.001) ? control.ActualWidth : control.Width;
+            var height = (Math.Abs(control.ActualHeight) > 0.001) ? control.ActualHeight : control.Height;
+
+            var name = GetRuleName(rules, new Size(width, height));
+            if (string.IsNullOrWhiteSpace(name)) return;
+
+            VisualStateManager.GoToState(control, name, true);
+        }
+
+        /*******************************************************************/
+
+
+        private static void EnsureWindowSizeChanged()
+        {
+            if (WindowSizeChangedHandled) return;
+            WindowSizeChangedHandled = true;
+
+            Window.Current.SizeChanged += (sender, args) =>
+            {
+                var rules = StyleRules ?? DefaultStyleRules;
+                if ((rules == null) || (rules.Count == 0)) return;
+
+                var name = GetRuleName(rules, args.Size);
+
+                foreach (var control in FrameworkElementsWithStyle)
+                    SetFrameworkStyle(control, name);
+            };
+        }
+
+
+        private static void SetFrameworkStyle(FrameworkElement frameworkElement, string name)
+        {
+            var styleName = GetStyle(frameworkElement);
+            if (string.IsNullOrWhiteSpace(styleName)) return;
+
+            Style style = null;
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                string modifiedStyleName = styleName + "." + name;
+                if (Application.Current.Resources.ContainsKey(modifiedStyleName))
+                    style = Application.Current.Resources[modifiedStyleName] as Style;
+            }
+            if (style == null)
+                style = Application.Current.Resources[styleName] as Style;
+
+            if (style != null)
+                frameworkElement.Style = style;
+        }
+
+
+        /*
+        private static void ValidateRules(IEnumerable<IDynamicRule> rules)
+        {
+            foreach (var rule in rules)
+            {
+                if (rule.IsTall && rule.IsWide)
+                    throw new ArgumentException("A rule cannot have both IsTall and IsWide");
+
+                if (rule.MinWidth > rule.MaxWidth)
+                    throw new ArgumentException("Rule MinWidth cannot be more than MaxWidth");
+
+                if (rule.MinHeight > rule.MaxHeight)
+                    throw new ArgumentException("Rule MinHeight cannot be more than MaxHeight");
+            }
+        }
+        */
+
+        private static string GetRuleName(IEnumerable<IDynamicRule> rules, Size size)
+        {
+            foreach (var rule in rules)
+            {
+                if (rule.IsMatch(size))
+                    return rule.Name;
+            }
+            return null;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,96 @@
-DynamicStyle
-============
+# DynamicStyle #
 
 Responsive layout for Universal Apps
+
+The DynamicStyle project has two functions.
+
+1. Automatically set the VisualState based on the size, shape, orientation of the screen
+2. Automatically set element Styles based on the size, shape, orientation of the screen
+
+### Why? ###
+**Control Styles** are also awesome. You can set things like foreground color, font sizes, margins and save them in a style to reuse.
+
+**Visual States** are awesome.  They allow you to make different layouts and styles based on named states.
+Maybe you have two basic states for your phone project.  Landscape and Portrait (apparently this is old terminology and we should be saying Wide and Tall)
+Or maybe you have 5 different states.  XWide, Wide, Default, Tall, Skinny
+
+**But**, in XWide Visual State you want to increase the font weight to be X, for Tall Visual State you want the font size to decrease.  When you have lots of controls on a screen with different visual states, the awesomeness of Visual States is broken because you have to change the properties for all of the controls for each of the states.  It also breaks control style because you have the values scattered between Style definitions and Visual States
+
+
+## Defining Control Styles ##
+OK, so you have defined an awesome style for your titles.
+
+``` xml
+<Style x:Key="Title" TargetType="TextBlock">
+    <Setter Property="FontSize" Value="32"/>
+</Style>
+```
+
+But when the screen is XWide, you want the font to be a bit bigger.
+``` xml
+<Style x:Key="Title.XWide" TargetType="TextBlock">
+    <Setter Property="FontSize" Value="46"/>
+</Style>
+```
+
+And when the app is snapped to the left, you want the font a little smaller.
+``` xml
+<Style x:Key="Title.Tall" TargetType="TextBlock">
+    <Setter Property="FontSize" Value="24"/>
+</Style>
+```
+
+Styles are soo awesome. You can set different margins, colors, content, .. any property you like, and store them in a named style.
+Using the above convention, you can store styles using the state name as a suffix (Title.XWide, Title.Tall)
+
+As Styles are typically defined globally (a very good practice), the definitions of the state properties are also stored globally.  The following is the default StyleRules, but you can override these with whatever you like. 
+
+``` csharp
+Dynamic.StyleRules = new List<IDynamicRule>
+{
+    new ShapeRule { Name = "XWide", IsWide = true, MinWidth = 1366 },
+    new ShapeRule { Name = "Wide", IsWide = true },
+    new ShapeRule { Name = "Tall", IsTall = true }
+};
+```
+
+The normal approach to setting a control style would be like this:
+``` xml
+<TextBlock Text="{Binding Title}" Style="{StaticResource Title}"/>
+``` 
+
+The DynamicStyle approach would look like this:
+``` xml
+<TextBlock Text="{Binding Title}" dynamic:Dynamic.Style="Title"/>
+``` 
+
+According to the rules above, when the screen is wider than 1366, the control will have the `Title.XWide` style applied.  When the app is snapped to the left, the control will have the `Title.Tall` style applied.  If it doesn't meet those two criteria, then the `Title` style is applied.
+
+
+
+## VisualState ##
+Maybe you have two basic visual states called Wide and Tall.  When you hold the screen in portrait, you want the Tall Visual state applied, and when in landscape, you want the Wide style applied.
+In the past you could monitor the orientation change, check the size and set the state.  Not a lot of code, but now you can define the rules as part of your XAML.
+
+``` xml
+<dynamic:Dynamic.VisualStateRules>
+    <dynamic:ShapeRule Name="Wide" IsWide="True"/>
+    <dynamic:ShapeRule Name="Tall" IsTall="True"/>
+</dynamic:Dynamic.VisualStateRules>
+```
+
+Maybe your Visual State is a little more fine grained.  Defining the rules for when to switch state is a lot quicker than code.
+``` xml
+<dynamic:Dynamic.VisualStateRules>
+    <dynamic:ShapeRule Name="XWide" IsWide="True" MinWidth="1366"/>
+    <dynamic:ShapeRule Name="Wide" IsWide="True"/>
+    <dynamic:ShapeRule Name="Skinny" IsTall="True" MaxWidth="320"/>
+    <dynamic:ShapeRule Name="Tall" IsTall="True"/>
+    <dynamic:ShapeRule Name="Default" />
+</dynamic:Dynamic.VisualStateRules>
+```
+
+Rules are evaluates top to bottom and the first that matches get applied.  
+
+Note: the last rule `Default` has no parameters, so will always be used if the other rules are not set.
+
